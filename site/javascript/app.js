0160f8d6e25c1bd06aa48d5a858456b3fb6dbb96
Andy Burke <aburke@bitflood.org>
2011-12-09 17:49:13 -0800

    Some templating boilerplate.

--- site/javascript/app.js
+++ site/javascript/app.js
@@ -1,13 +1,35 @@
 ;(function($) {
+    var templateCache = {};
+    
+    function renderTemplate( elementSelector, template, data ) {
+        if ( templateCache[ template ] )
+        {
+            $(elementSelector).html( Mustache.to_html( templateCache[ template ], data ) );
+        }
+        else
+        {
+            $.ajax({
+                url: template,
+                dataType: "text",
+                success: function( contents ) {
+                    templateCache[ template ] = contents;
+                    renderTemplate( elementSelector, template, data );
+                }
+            });
+        }
+    }
+
   var app = $.sammy('#main', function() {
     this.debug = true;
 
+    this.templates = {};
+    
     this.get('#/', function() {
       $('#main').text('');
     });
 
     this.get('#/test', function() {
-      $('#main').text('Hello World');
+        renderTemplate( '#main', "/templates/organization.mustache", { name: 'Test', email: 'test@test.com' } );
     });
   });
 
