cfc21436cb1665cae39701371598ea1afa7025fc
Andy Burke <aburke@bitflood.org>
2011-12-26 22:25:29 -0800

    Get rid of Organizations from the site. Improve profiles and missing profile handling.

--- site/javascript/app.js
+++ site/javascript/app.js
@@ -27,7 +27,6 @@ function SetActivePage( page )
 }
 
 var g_TemplateCache = {};
-var g_OrganizationCache = {};
 var g_ContextCache = {};
 
 function renderTemplate( elementSelector, template, data, callback ) {
@@ -110,133 +109,85 @@ var app = Sammy( function() {
             url: apiServer + '/User/' + this.params[ 'hash' ],
             type: 'GET',
             dataType: 'json',
-            success: function( data ) {
-                $('#main').toggleLoading();
-                renderTemplate( '#main', '/templates/user.mustache', data );
-            },
-            error: function( response, status, error ) {
-                $('#main').toggleLoading();
-                console.log( error );
-            }
-        });
-    });
-    
-    this.get( '#/ManageOrganizations', function() {
-        SetActivePage( 'manageorganizations' );
-        $('#main').toggleLoading();
-        
-        $.ajax({
-            url: apiServer + '/Organizations',
-            type: 'GET',
-            dataType: 'json',
-            success: function( organizations ) {
-                $('#main').toggleLoading();
-                
-                for ( var index = 0; index < organizations.length; ++index )
-                {
-                    g_OrganizationCache[ organizations[ index ]._id ] = organizations[ index ];
-                }
-                
-                renderTemplate( '#main', '/templates/manageorganizations.mustache', { 'organizations': organizations } );
-            },
-            error: function( response, status, error ) {
-                $('#main').toggleLoading();
-                console.log( error );
-            }
-        });
-    });
-    
-    this.get( '#/CreateOrganization', function() {
-        renderTemplate( '#main', '/templates/createorganization.mustache' ); 
-    });
-    
-    this.get( '#/ManageOrganization/:organizationId', function () {
-        $( '#main' ).toggleLoading();
-        
-        var organizationId = this.params[ 'organizationId' ];
-        $.ajax({
-            url: apiServer + '/Organization/' + organizationId,
-            type: 'GET',
-            dataType: 'json',
-            success: function( organization ) {
-                $( '#main' ).toggleLoading();
-                
-                g_OrganizationCache[ organization._id ] = organization;
-                
-                renderTemplate( '#main', '/templates/manageorganization.mustache', organization, function() {
-                    $( '#contexts' ).toggleLoading();
+            success: function( user ) {
+                renderTemplate( '#main', '/templates/user.mustache', { 'user': user }, function() {
+                    $( '#main' ).toggleLoading();
+
+                    $( '#achievements' ).toggleLoading();
                     $.ajax({
-                        url: apiServer + '/Organization/' + organizationId + '/Contexts',
+                        url: apiServer + '/User/' + user._id + '/Achievements',
                         type: 'GET',
                         dataType: 'json',
-                        success: function( contexts ) {
-                            $( '#contexts' ).toggleLoading();
-                            
-                            for ( var contextIndex = 0; contextIndex < contexts.length; ++contextIndex )
-                            {
-                                g_ContextCache[ contexts[ contextIndex ]._id ] = contexts[ contextIndex ];
-                            }
-                            
-                            renderTemplate( '#contexts', '/templates/contextlist.mustache', { 'organization': organization, 'contexts': contexts } );
+                        success: function( achievements ) {
+                            renderTemplate( '#achievements', '/templates/achievementlist.mustache', { 'achievements': achievements }, function () {
+                                $( '#achievements' ).toggleLoading();
+                            });
                         },
                         error: function( response, status, error ) {
-                            $( '#contexts' ).toggleLoading();
-                            console.log( error );
+                            $( '#achievements' ).toggleLoading();
                         }
                     });
+
+                });
+            },
+            error: function( response, status, error ) {
+                if ( response.status == 404 )
+                {
+                    renderTemplate( '#main', '/templates/user.mustache', { 'user': {} }, function() {
+                        $( '#main' ).toggleLoading();
+                    });
+                }
+                else
+                {
+                    $( '#main' ).toggleLoading();
+                    console.log( error );
+                }
+            }
+        });
+    });
+
+    this.get( '#/ManageContexts', function () {
+        $( '#main' ).toggleLoading();
+        
+        $.ajax({
+            url: apiServer + '/Contexts',
+            type: 'GET',
+            dataType: 'json',
+            success: function( contexts ) {
+                g_ContextCache = {};
+                for ( var contextIndex = 0; contextIndex < contexts.length; ++contextIndex )
+                {
+                    g_ContextCache[ contexts[ contextIndex ]._id ] = contexts[ contextIndex ];
+                }
+                
+                renderTemplate( '#main', '/templates/managecontexts.mustache', { 'contexts': contexts }, function() {
+                    $( '#main' ).toggleLoading();
                 });
             },
             error: function( response, status, error ) {
                 $( '#main' ).toggleLoading();
                 console.log( error );
             }
+        })
+    });
+    
+    this.get( '#/CreateContext', function() {
+        $( '#main' ).toggleLoading();
+        renderTemplate( '#main', '/templates/createcontext.mustache', null, function () {
+            $( '#main' ).toggleLoading();
         });
     });
     
-    this.get( '#/CreateContext/:organizationId', function() {
+    this.get( '#/ManageContext/:contextId', function () {
         $( '#main' ).toggleLoading();
         
-        var organizationId = this.params[ 'organizationId' ];
-        var organization = g_OrganizationCache[ organizationId ];
-        
-        if ( !organization )
-        {
-            $.ajax({
-                url: apiServer + '/Organization/' + organizationId,
-                type: 'GET',
-                dataType: 'json',
-                success: function( organization ) {
-                    g_OrganizationCache[ organization._id ] = organization;
-                    
-                    renderTemplate( '#main', '/templates/createcontext.mustache', { 'organization': organization }, function() {
-                        $( '#main' ).toggleLoading(); 
-                    });
-                },
-                error: function( response, status, error ) {
-                    $( '#main' ).toggleLoading();
-                    console.log( error );
-                }
-            });
-        }
-        else
-        {
-            renderTemplate( '#main', '/templates/createcontext.mustache', { 'organization': organization }, function() {
-                $( '#main' ).toggleLoading(); 
-            });
-        }
-    });
-
-    this.get( '#/ManageOrganization/:organizationId/ManageContext/:contextId', function () {
-        $( '#main' ).toggleLoading();
-        
-        var organizationId = this.params[ 'organizationId' ];
         var contextId = this.params[ 'contextId' ];
         var context = g_ContextCache[ contextId ];
         
         if ( !context )
         {
             $.ajax({
-                url: apiServer + '/Organization/' + organizationId + '/Context/' + context._id,
+                url: apiServer + '/Context/' + context._id,
                 type: 'GET',
                 dataType: 'json',
                 success: function( context ) {
@@ -440,125 +391,6 @@ $('.reset-account-button').live( 'click', function( event ) {
     }
 });
 
-$('.button-create-organization').live( 'click', function( event ) {
-    event.preventDefault();
-    var form = $(this).parents( 'form:first' );
-
-    $(form).toggleLoading();
-
-    var name = $(form).find( '#name' ).val();
-    var url = $(form).find( '#url' ).val();
-    var description = $(form).find( '#description' ).val();
-
-    $.ajax({
-        url: apiServer + '/Organization',
-        type: 'POST',
-        data: JSON.stringify({
-            'name': name,
-            'url': url,
-            'description': description
-        }),
-        dataType: 'json',
-        contentType: 'application/json',
-        cache: false,
-        success: function( organization ) {
-            $(form).toggleLoading();
-            app.setLocation( '#/ManageOrganizations' );
-        },
-        error: function( response, status, error ) {
-            $(form).toggleLoading();
-            console.log( error );
-        }
-    });
-});
-
-$('.update-organization-button').live( 'click', function( event ) {
-    event.preventDefault();
-    var button = this;
-    var form = $(this).parents( 'form:first' );
-
-    var toBeUpdated = {};
-
-    var organizationId = $(form).find( '#id' ).val();
-    var organization = g_OrganizationCache[ organizationId ];
-    
-    if ( !organization )
-    {
-        console.log( "Should have the organization cached at this point..." );
-        return;
-    }
-
-    var name = $(form).find( "#name" ).val();
-    if ( name != organization.name )
-    {
-        toBeUpdated.name = name;
-    }
-    
-    var url = $(form).find( "#url" ).val();
-    if ( url != organization.url )
-    {
-        toBeUpdated.url = url;
-    }
-
-    var description = $(form).find( "#description" ).val();
-    if ( description != organization.description )
-    {
-        toBeUpdated.description = description;
-    }
-    
-    // only send if toBeUpdated has at least one key
-    for ( var key in toBeUpdated )
-    {
-        $(button).button( 'loading' );
-        $(form).toggleLoading();
-
-        $.ajax({
-            url: apiServer + '/Organization/' + organizationId,
-            type: 'PUT',
-            data: JSON.stringify( toBeUpdated ),
-            dataType: 'json',
-            contentType: 'application/json',
-            success: function( organization ) {
-                g_OrganizationCache[ organization._id ] = organization;
-                $(form).toggleLoading();
-                $(button).button( 'complete' );
-                setTimeout( function() {
-                    $(button).button( 'reset' );
-                }, 2000 );
-            },
-            error: function( response, status, error ) {
-                $(form).toggleLoading();
-                console.log( error );
-                $(button).button( 'reset' );
-            }
-        });
-
-        break; // we break, no matter what, because we just wanted to see if there was a key in toBeUpdated
-    }
-});
-
-$('.reset-organization-button').live( 'click', function( event ) {
-    event.preventDefault();
-    var form = $(this).parents( 'form:first' );
-
-    // TODO: prompt for confirmation, maybe use bootstrap-modal.js?
-    
-    var organizationId = $(form).find( '#id' ).val();
-    var organization = g_OrganizationCache[ organizationId ];
-    
-    if ( !organization )
-    {
-        console.error( 'Organization should be set.' );
-        return;
-    }
-    
-    for ( var key in organization )
-    {
-        $(form).find( '#' + key ).val( organization[ key ] );
-        $(form).find( '#' + key ).html( organization[ key ] );
-    }
-});
-
 $('.button-create-context').live( 'click', function( event ) {
     event.preventDefault();
     var form = $(this).parents( 'form:first' );
@@ -568,10 +400,9 @@ $('.button-create-context').live( 'click', function( event ) {
     var name = $(form).find( '#name' ).val();
     var url = $(form).find( '#url' ).val();
     var description = $(form).find( '#description' ).val();
-    var organizationId = $(form).find( '#organizationId' ).val();
     
     $.ajax({
-        url: apiServer + '/Organization/' + organizationId + '/Context',
+        url: apiServer + '/Context',
         type: 'POST',
         data: JSON.stringify({
             'name': name,
@@ -586,7 +417,7 @@ $('.button-create-context').live( 'click', function( event ) {
             
             g_ContextCache[ context._id ] = context;
             
-            app.setLocation( '#/ManageOrganization/' + organizationId );
+            app.setLocation( '#/ManageContext/' + context._id );
         },
         error: function( response, status, error ) {
             $(form).toggleLoading();
@@ -605,29 +436,26 @@ $('.update-context-button').live( 'click', function( event ) {
     var contextId = $(form).find( '#id' ).val();
     var context = g_ContextCache[ contextId ];
     
-    var organizationId = $(form).find( '#organizationId' ).val();
-    var organization = g_OrganizationCache[ organizationId ];
-    
-    if ( !context || !organization )
+    if ( !context )
     {
         console.log( 'Cache Error' );
         return;
     }
 
     var name = $(form).find( "#name" ).val();
-    if ( name != organization.name )
+    if ( name != context.name )
     {
         toBeUpdated.name = name;
     }
     
     var url = $(form).find( "#url" ).val();
-    if ( url != organization.url )
+    if ( url != context.url )
     {
         toBeUpdated.url = url;
     }
 
     var description = $(form).find( "#description" ).val();
-    if ( description != organization.description )
+    if ( description != context.description )
     {
         toBeUpdated.description = description;
     }
@@ -639,7 +467,7 @@ $('.update-context-button').live( 'click', function( event ) {
         $(form).toggleLoading();
 
         $.ajax({
-            url: apiServer + '/Organization/' + organizationId + '/Context/' + contextId,
+            url: apiServer + '/Context/' + contextId,
             type: 'PUT',
             data: JSON.stringify( toBeUpdated ),
             dataType: 'json',
